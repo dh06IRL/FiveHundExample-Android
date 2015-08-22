0311487f8bd91f49d2fad18e515ae6465fe356f5
David Hodge <dhodge.jr229@gmail.com>
2015-08-21 21:37:26 -0400

    move shared backpress logic to base activity

--- app/src/main/java/com/david/github/activity/BaseActivity.java
+++ app/src/main/java/com/david/github/activity/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.david.github.activity;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -8,4 +9,13 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class BaseActivity extends AppCompatActivity {
 
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAfterTransition();
+        }else{
+            finish();
+        }
+    }
 }
